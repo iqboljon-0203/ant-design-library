08b1ff8d8b585ffb651b89376babcfe7ef668b5d
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2023-03-31 15:20:51 +0500

    Tooltip component updated

--- src/components/Tooltips/Tooltip/index.jsx
+++ src/components/Tooltips/Tooltip/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { ToolTipButton, ToolTipContent } from "./style";
 
-export const ToolTip = ({ toolBgColor, children, ...res }) => {
+export const ToolTip = ({ toolBgColor,toolPos, children, ...res }) => {
   return (
     <ToolTipButton {...res}>
       {children}
-      <ToolTipContent toolBgColor={toolBgColor} {...res}>
-        {toolBgColor ? toolBgColor : "Content tooltip bottom"}
+      <ToolTipContent toolPos={toolPos} toolBgColor={toolBgColor} {...res}>
+        {toolBgColor ? toolBgColor : `Content tooltip ${toolPos} `}
       </ToolTipContent>
     </ToolTipButton>
   );
